eb5e584dff34515a80b1d3040efdc0f61e356a00
NickyH <nickyhughes00@gmail.com>
2013-02-19 09:16:52 -0500

    Added rails

---
+++ nickyportfolio/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nickyportfolio::Application.config.secret_token = '0906fcc81827871797d0686fee1a1a9edd5f21144e6d21790ebd5be3fa81c695eaf44d9a49286ea217862704788e8b3a8c1a260a52944530ceb93b58e1adebf9'
---
+++ nickyportfolio/config/routes.rb
@@ -0,0 +1,3 @@
+Nickyportfolio::Application.routes.draw do
+ root :to => 'home#index'
+end
